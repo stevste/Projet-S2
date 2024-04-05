18188f432687f65834038168b98074a50ef41307
Stevste <stevansagot12@gmail.com>
2024-04-05 11:17:27 +0200

    Hello World!

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+import numpy as np
+from tkinter import *
+import tkinter as tk
+import matplotlib as plt
+
+window = tk.Tk()
+window.title('test')
+window.geometry('1024x768')
+window.config(bg='#F26849')
+window.grid()
+
+tk.Button(window, text="Quit", command=window.destroy).grid(column=0, row=1)
+window.mainloop()
