2b3eb25d62b01981ec5d8629b10145602081fb76
Stevste <stevansagot12@gmail.com>
2024-04-08 20:45:36 +0200

    creation de createGraph

--- graph.py
+++ graph.py
@@ -1,17 +1,26 @@
 import random as rand
 import matplotlib.pyplot as plt
 
+def createGraph(data):
+    fig = plt.figure(figsize=(6, 4), dpi=100)
+    axes = fig.add_subplot()
+    axes.set_title('Évolution des températures')
+    axes.set_xlabel("Date")
+    axes.set_ylabel("Températures")
+
+    index = 0
+    for i in data:
+        axes.plot(i.keys(), i.values(), '.-', label='zone '+str(index))
+        index += 1
+
+    axes.legend()
+
+    return fig
+
 data = []
 
 for i in range (5):
     data.append(dict([("2024-4-8", rand.randint(-5, 35)), ("2024-4-9", rand.randint(-5, 35)), ("2024-4-10", rand.randint(-5, 35)), ("2024-4-11", rand.randint(-5, 35)), ("2024-4-12", rand.randint(-5, 35)), ("2024-4-13", rand.randint(-5, 35))]))
 
-date = data[0].keys()
-
-for i in data:
-    plt.plot(date, i.values(), label=str())
-
-plt.xlabel("Date")
-plt.ylabel("Températures")
-
-plt.show()
+fig = createGraph(data)
+fig.show()
