f9cff8419bb1739538ea6b6f599175ca5a51d9f0
Stevste <stevansagot12@gmail.com>
2024-04-15 10:41:29 +0200

    Revert "réecriture du code de fenetre en fonction et base du layout"
    This reverts commit 7eb2c34ab67de91f17306e33d6f8c9d1079a6dce.

---
+++ graph.py
@@ -0,0 +1,18 @@
+import random as rand
+import matplotlib.pyplot as plt
+
+def createGraph(data):
+    fig = plt.figure(figsize=(6, 4), dpi=100)
+    axes = fig.add_subplot()
+    axes.set_title('Évolution des températures')
+    axes.set_xlabel("Date")
+    axes.set_ylabel("Températures")
+
+    index = 0
+    for i in data:
+        axes.plot(i.keys(), i.values(), '.-', label='zone '+str(index+1))
+        index += 1
+
+    axes.legend()
+
+    return fig
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+import tkinter as tk
+import random as rand
+
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+
+import graph
+
+data = []
+
+for i in range (5):
+    data.append(dict([("2024-4-8", rand.randint(-5, 35)), ("2024-4-9", rand.randint(-5, 35)), ("2024-4-10", rand.randint(-5, 35)), ("2024-4-11", rand.randint(-5, 35)), ("2024-4-12", rand.randint(-5, 35)), ("2024-4-13", rand.randint(-5, 35))]))
+
+window = tk.Tk()
+window.title('test')
+window.grid()
+
+figure = graph.createGraph(data)
+figure_canvas = FigureCanvasTkAgg(figure, window)
+
+tk.Button(window, text="Quit", command=window.destroy).grid(column=0, row=1)
+graphWidget = figure_canvas.get_tk_widget().grid(column=0, row=0)
+
+window.mainloop()
