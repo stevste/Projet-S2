dd4200447115ecb708c8bf1d6f64b0fe9118989d
ragnarand <contin.jeremy001@gmail.com>
2024-05-06 11:32:50 +0200

    correction des betises

--- data.py
+++ data.py
@@ -1,20 +1,19 @@
 import sqlite3 as sql
 
-connexion = sql.connect("C:\\Users\\conti\\Desktop\\info\\s2\\projet arrosage\\Projet-S2\\bdd test.db")
+connexion = sql.connect("bdd.db")
 curs = connexion.cursor()
 
 def temperatures(dateDebut : int=1, delta : int=1) -> list :
-    curs.execute("SELECT * FROM TEMPERATURE WHERE date BETWEEN " + str(dateDebut) + " AND "+ str(dateDebut + delta) + " ORDER BY date ASC")
+    curs.execute("SELECT * FROM TEMPERATURE WHERE date BETWEEN " + str(dateDebut) + " AND "+ str(dateDebut + delta) + " ORDER BY idCapteur ASC")
     liste = curs.fetchall()
-    dico = {}
-    sousliste = []
-    for i in range(delta) :
-        for elt in liste :
-            if elt[1] == dateDebut + i :
-                sousliste.append(elt[2])
-        dico[dateDebut + i] = sousliste
-        sousliste = []
-    return dico
+    dicoList = []
+    for i in range(20) :
+        newDico = {}
+        for i in range(delta):
+            newDico.update(liste[i+delta][1], liste[i+delta])
+
+        dicoList.append(newDico)
+    return dicoList
 
 print(temperatures())
 
